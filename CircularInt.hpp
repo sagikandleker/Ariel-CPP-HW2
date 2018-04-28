3ac5f96ac8afbdaf311b27236c752a7f4424d976
Sagi Saada <33149808+sagikandleker@users.noreply.github.com>
2018-04-28 13:38:44 +0300

    Update CircularInt.hpp

--- CircularInt.hpp
+++ CircularInt.hpp
@@ -25,8 +25,8 @@ public:
 	CircularInt& operator*=(const int);
 	CircularInt& operator*=(const CircularInt& h);
 
-	//CircularInt& operator/=(const int);
-	//CircularInt& operator/=(const CircularInt& h);
+	CircularInt& operator/=(const int);
+	CircularInt& operator/=(const CircularInt& h);
 
 	CircularInt operator--(const int);
 	CircularInt& operator--();
@@ -36,10 +36,6 @@ public:
 
 	CircularInt& operator=(const int);
 	CircularInt& operator=(const CircularInt& h);
-
-	//vector<int> operator/(const int num);
-	//vector<int> operator/(const CircularInt& h);
-
 
 	friend int operator >= (const CircularInt& h, int num);
 	friend int operator >= (int num, const CircularInt& h);
@@ -91,9 +87,9 @@ public:
 	friend CircularInt operator / (const int num,const CircularInt & h);
 	friend CircularInt operator / (const CircularInt & a,const CircularInt & b);
 
-	friend CircularInt operator /= (const CircularInt & h, const int num);
+	/*friend CircularInt operator /= (const CircularInt & h, const int num);
 	friend CircularInt operator /= (const int num,const CircularInt & h);
-	friend CircularInt operator /= (const CircularInt & a,const CircularInt & b);
+	friend CircularInt operator /= (const CircularInt & a,const CircularInt & b);*/
 
 	friend ostream& operator<<(ostream& os, const CircularInt &m);
 	friend istream& operator>>(istream& os, CircularInt &m);
@@ -209,86 +205,13 @@ inline CircularInt operator/(const CircularInt & h, const int num){
 
 inline CircularInt operator/(const int num,const CircularInt & h){
 	CircularInt temp(h);
-	num /= temp;
+	temp /= num;
 	return temp;
 
 }
 
 inline CircularInt operator/(const CircularInt & a,const CircularInt & b){
-	CircularInt temp(a);
-	temp /= b.now;
-	return temp;
-}
-
-inline CircularInt operator/=(const CircularInt & h, const int num){
-	CircularInt Object(h);
-	for(int i = Object.min; i < Object.max; i++)
-	{
-		int temp = i*num;
-		while(temp>Object.max){
-			temp-=Object.max;
-		}
-
-		while(temp<Object.min){
-			temp+=Object.max;
-		}
-
-		if(temp == Object.now)
-		{
-			Object.now = i;
-			return Object;
-		}
-	}
-
-	throw string("There is no number x in {"+to_string(Object.min)+ ","+ to_string(Object.max)+"} such that x*" + to_string(num)+"="+to_string(Object.now));
-}
-
-inline CircularInt operator/=(const int num, const CircularInt & h){
-	CircularInt Object(h);
-	for(int i = Object.min; i < Object.max; i++)
-	{
-		int temp = i*Object.now;
-		while(temp>Object.max){
-			temp-=Object.max;
-		}
-
-		while(temp<Object.min){
-			temp+=Object.max;
-		}
-
-		if(temp == num)
-		{
-			Object.now = i;
-			return Object;
-		}
-	}
-
-	throw string("There is no number x in {"+to_string(Object.min)+ ","+ to_string(Object.max)+"} such that x*" + to_string(num)+"="+to_string(Object.now));
-}
-
-
-inline CircularInt operator/=(const CircularInt & a,const CircularInt & b){
-	CircularInt Object1(a);
-	CircularInt Object2(b);
-	for(int i = Object1.min; i < Object1.max; i++)
-	{
-		int temp = i*Object2.now;
-		while(temp>Object1.max){
-			temp-=Object1.max;
-		}
-
-		while(temp<Object1.min){
-			temp+=Object1.max;
-		}
-
-		if(temp == Object1.now)
-		{
-			Object1.now = i;
-			return Object1;
-		}
-	}
-
-	throw string("There is no number x in {"+to_string(Object1.min)+ ","+ to_string(Object1.max)+"} such that x*" + to_string(Object2.now)+"="+to_string(Object1.now));
+	return a / b.now;
 }
 
 inline int operator>(const CircularInt& h, int num){
